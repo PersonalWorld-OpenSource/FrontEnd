cfcbfcfe28d6c098cae4f328afd179484b0d4e0a
AleSilva04 <81649035+AleSilva04@users.noreply.github.com>
2022-05-02 14:59:57 -0500

    feat: "choose and pay plan"
    added choose plan and pay subscriptions features

---
+++ src/views/choose-plan.vue
@@ -0,0 +1,69 @@
+<template>
+
+  <h2 class="text-center mt-3">Escoger Plan</h2>
+  <div class="d-flex align-center mt-16">
+
+    <v-card width="300" class="ma-auto">
+      <h3 class="text-center text-blue-darken-2 mt-2" >Derecho Civil</h3>
+      <v-card-text>
+        <p>Contrary to popular belief alive vertain world</p>
+        <p>Contrary to popular belief</p>
+        <p>Contrary to popular belief</p>
+        <router-link class="text-decoration-none text-white" to=""><v-btn flat color="#1C58AE" class="mt-2" >
+          Pagar
+        </v-btn></router-link>
+      </v-card-text>
+    </v-card>
+
+    <v-card width="300" class="ma-auto">
+      <h3 class="text-center text-blue-darken-2 mt-2" >Derecho Familiar</h3>
+
+      <v-card-text>
+        <p>Contrary to popular belief alive vertain world</p>
+        <p>Contrary to popular belief</p>
+        <p>Contrary to popular belief</p>
+        <router-link class="text-decoration-none text-white" to=""><v-btn flat color="#1C58AE" class="mt-2" >
+          Pagar
+        </v-btn></router-link>
+      </v-card-text>
+    </v-card>
+
+    <v-card width="300" class="ma-auto">
+      <h3 class="text-center text-blue-darken-2 mt-2" >Derecho Mercantil</h3>
+
+      <v-card-text>
+        <p>Contrary to popular belief alive vertain world</p>
+        <p>Contrary to popular belief</p>
+        <p>Contrary to popular belief</p>
+        <router-link class="text-decoration-none text-white" to=""><v-btn flat color="#1C58AE" class="mt-2" >
+          Pagar
+        </v-btn></router-link>
+      </v-card-text>
+    </v-card>
+
+    <v-card width="300" class="ma-auto">
+      <h3 class="text-center text-blue-darken-2 mt-2" >Derecho Penal</h3>
+
+      <v-card-text>
+        <p>Contrary to popular belief alive vertain world</p>
+        <p>Contrary to popular belief</p>
+        <p>Contrary to popular belief</p>
+        <router-link class="text-decoration-none text-white" to=""><v-btn flat color="#1C58AE" class="mt-2" >
+          Pagar
+        </v-btn></router-link>
+      </v-card-text>
+    </v-card>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "choose-plan",
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/pay-subscription.vue
@@ -0,0 +1,34 @@
+<template >
+
+  <h2 class="text-center mt-3 ">Pagar Suscripcion</h2>
+  <v-card class="mx-auto mt-5 py-2"  width="400">
+    <v-form class="px-3">
+      <v-text-field label="Numero Tarjeta"></v-text-field>
+      <v-text-field label="Fecha Expiracion"></v-text-field>
+      <v-text-field label="CVV"></v-text-field>
+      <v-text-field label="Titular"></v-text-field>
+      <v-btn flat color="#1C58AE" class="text-white" @click="successful">
+        Pagar
+      </v-btn>
+    </v-form>
+
+  </v-card>
+</template>
+
+<script>
+
+export default {
+  name: "pay-subscription",
+
+  methods:{
+    successful(){
+      alert("Pago exitoso");
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
