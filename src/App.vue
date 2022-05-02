61d2a88e00687c1691e0c0615076efefd48a42fc
djalmadioses <dioses1302@gmail.com>
2022-05-02 16:30:21 -0500

    fix: "router connections"
    added the router connections

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,6 @@
   <v-app className="bg-grey-lighten-3">
     <v-main>
 
-<<<<<<< Updated upstream
-=======
       <Header></Header>
 
       <router-view></router-view>
@@ -11,36 +9,20 @@
       <Footer></Footer>
 
 
->>>>>>> Stashed changes
     </v-main>
   </v-app>
 </template>
 
 <script>
-<<<<<<< Updated upstream
-
-=======
 import Header from './components/header.vue'
 import Footer from './components/footer.vue'
->>>>>>> Stashed changes
 
 export default {
   name: 'App',
 
   components: {
-<<<<<<< Updated upstream
-
-  },
-
-  data: () => ({
-    //
-  }),
-}
-</script>
-=======
     Header,
     Footer,
   },
 }
 </script>
->>>>>>> Stashed changes
