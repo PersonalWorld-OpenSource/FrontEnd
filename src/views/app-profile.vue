47b6cc23d7b1edc363cc0aa6f1b7ef33dd50eac8
JoaquinSM13 <u201910511@upc.edu.pe>
2022-05-22 13:27:28 -0500

    feat: profile settings with data

--- src/views/app-profile.vue
+++ src/views/app-profile.vue
@@ -1,8 +1,12 @@
 <template>
   <v-container fluid>
-    <v-row class="justify-center mt-16" v-for="(oneProfile,index) in profiles.splice(0,1)">
+    <v-row
+      class="justify-center mt-16"
+      v-for="(oneProfile, index) in profiles.splice(0, 1)"
+      :key="index"
+    >
       <v-col cols="4" md="2">
-        <h2 class="text-md-center mb-5">{{ profiles[1].name }}</h2>
+        <h2 class="text-md-center mb-5">{{ oneProfile.name }}</h2>
         <v-img
           max-height="600"
           max-width="600"
@@ -11,7 +15,7 @@
       </v-col>
       <v-col cols="8" md="7">
         <p class="rounded-lg bg-white pa-4 ma-11">
-          {{ profiles[1].description }}
+          {{ oneProfile.description }}
         </p>
         <div class="text-md-center">
           <router-link class="text-decoration-none" to="/editar-perfil">
